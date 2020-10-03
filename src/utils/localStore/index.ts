5cbb3c3cf5a70b5dd880c40a6b6aacd9190b8aed
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-03 16:06:04 +1300

    Add local store wrapper

---
+++ src/utils/localStore/index.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs-extra'
+import { config } from 'shelljs'
+import {configFilePath, notesFilePath} from '../constants'
+
+// wrapper for manipulate data on
+//  .devnotes/config.json
+//  .devnotes/notes.md
+
+const getProject = ():any => {
+  fs.ensureFileSync(configFilePath)
+
+  const localStoreData = fs.readJsonSync(configFilePath)
+  return localStoreData.project || {}
+}
+
+const setProject = (project: any) => {
+  fs.ensureFileSync(configFilePath)
+
+  const data = fs.readJsonSync(configFilePath)
+  const fresh = {...data.project, ...project}
+
+  fs.writeJsonSync(configFilePath, {
+    ...data,
+    project: fresh
+  })
+
+  return fresh
+}
+
+const getNotes = () => (fs.readFileSync(notesFilePath, 'utf8'))
+
+export {
+  getProject,
+  setProject,
+  getNotes
+}

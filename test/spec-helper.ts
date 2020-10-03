b28d0f5fc2eb9b24c296e3ae3aec715f34ac6cd1
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-04 11:28:54 +1300

    Add doc

--- test/spec-helper.ts
+++ test/spec-helper.ts
@@ -1,6 +1,11 @@
 import * as shell from 'shelljs'
 import {rootDir} from '../src/utils/constants'
 
+// This file is being hooked on mocha throught mocha.opts
+//
+// It sets the current test dir as '<root>/test/test_root_porject'
+//
+// Every test will run on that path.
 const clean = () => {
   shell.rm('-rf', rootDir, '.gitignore')
 }
